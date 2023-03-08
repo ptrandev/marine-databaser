d0d737adf1ffb8d2313d6127ad847f6da1b65d05
Phillip Tran <ptrandev@protonmail.com>
2023-03-07 23:59:51 -0500

    mui and react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './pages/App'
 import './samples/node-api'
-import './index.scss'
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import CssBaseline from '@mui/material/CssBaseline';
+
+import { createHashRouter, RouterProvider } from 'react-router-dom'
+
+const router = createHashRouter([
+  {
+    path: '/',
+    element: <App/>,
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <CssBaseline />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
 
