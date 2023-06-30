8dc7b7de60fe40dcb7aca7eea1480fd782aa9f46
Phillip Tran <ptrandev@protonmail.com>
2023-06-30 00:37:15 -0400

    working

--- src/hooks/useSpliceVideo.ts
+++ src/hooks/useSpliceVideo.ts
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import SpliceVideoContext, { SpliceVideoContextValue } from '../contexts/SpliceVideoContext';
+import SpliceVideoContext, { SpliceVideoContextValue } from '../contexts/SpliceVideo';
 
 const useSpliceVideo = (): SpliceVideoContextValue => useContext(SpliceVideoContext);
 
