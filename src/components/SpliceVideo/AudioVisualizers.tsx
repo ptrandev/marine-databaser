c347cc9073d9cd2939012cf60e8ee77139a8a9d4
Phillip Tran <ptrandev@protonmail.com>
2024-01-30 22:53:46 -0500

    added delete and modify functionality on waveform itself

--- src/components/SpliceVideo/AudioVisualizers.tsx
+++ src/components/SpliceVideo/AudioVisualizers.tsx
@@ -3,10 +3,11 @@ import WavesurferPlayer from '@wavesurfer/react'
 import SpectrogramPlugin from "wavesurfer.js/dist/plugins/spectrogram.js"
 import TimelinePlugin from 'wavesurfer.js/dist/plugins/timeline.js'
 import RegionPlugin from 'wavesurfer.js/dist/plugins/regions.js'
-import { Box, Slider, Stack, Typography } from '@mui/material'
+import { Box, IconButton, Slider, Stack, Typography } from '@mui/material'
 import { ipcRenderer } from 'electron'
 import colormap from 'colormap'
 import useSpliceVideo from '@/hooks/useSpliceVideo'
+import { Delete } from '@mui/icons-material'
 
 const colors = colormap({
   colormap: 'hot',
@@ -16,7 +17,7 @@ const colors = colormap({
 })
 
 const AudioVisualizers: FC = () => {
-  const { selectedVideo, videoRef, splicePoints } = useSpliceVideo()
+  const { selectedVideo, videoRef, splicePoints, modifySplicePoint, deleteSplicePoint } = useSpliceVideo()
   
   const [zoom, setZoom] = useState<number>(1)
   const [frequencyMax, setFrequencyMax] = useState<number>(22_050)
@@ -32,12 +33,27 @@ const AudioVisualizers: FC = () => {
     wsRegions.clearRegions()
 
     splicePoints.forEach(([start, end], i) => {
-      wsRegions.addRegion({
+      const deleteButton = document.createElement('div')
+      deleteButton.innerHTML = `<button style="background: none; border: none; cursor: pointer;" title="Delete"><svg width="24" height="24" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"/></svg></button>`
+      
+      deleteButton.onclick = (e) => {
+        deleteSplicePoint([start, end])
+        e.stopPropagation()
+      }
+
+      const region = wsRegions.addRegion({
         start: start,
         end: end,
         color: 'rgba(0, 0, 255, 0.1)',
-        drag: false,
-        resize: false,
+        drag: true,
+        resize: true,
+        id: `${start} ${end}`,
+        content: deleteButton,
+      })
+
+      region.on('update-end', () => {
+        const [start, end] = region.id.split(' ').map(Number)
+        modifySplicePoint([start, end], [region.start, region.end])
       })
     })
   }, [splicePoints, wsRegions])
