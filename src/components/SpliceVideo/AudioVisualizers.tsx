2be9ab8f1e49602a6442c977a0f5cade659d3c5d
Phillip Tran <ptrandev@protonmail.com>
2024-02-06 00:02:56 -0500

    now able to rename splice region

--- src/components/SpliceVideo/AudioVisualizers.tsx
+++ src/components/SpliceVideo/AudioVisualizers.tsx
@@ -3,12 +3,13 @@ import WavesurferPlayer from '@wavesurfer/react'
 import SpectrogramPlugin from "wavesurfer.js/dist/plugins/spectrogram.js"
 import TimelinePlugin from 'wavesurfer.js/dist/plugins/timeline.js'
 import RegionPlugin, { Region } from 'wavesurfer.js/dist/plugins/regions.js'
-import { Box, IconButton, Slider, Stack, Typography } from '@mui/material'
+import { Box, IconButton, Slider, Stack, Typography, TextField, Button, Grid } from '@mui/material'
 import { ipcRenderer } from 'electron'
 import colormap from 'colormap'
 import useSpliceVideo from '@/hooks/useSpliceVideo'
 import { Delete, Loop, PlayArrow, Pause, Edit } from '@mui/icons-material'
 import { SpliceRegion } from '../../../shared/types'
+import Modal from '../Modal'
 
 const COLORS = colormap({
   colormap: 'hot',
@@ -18,7 +19,7 @@ const COLORS = colormap({
 })
 
 const REGION_COLOR = 'rgba(0, 0, 255, 0.1)'
-const SELECTED_REGION_COLOR = 'rgba(0, 0, 255, 0.25)'
+const SELECTED_REGION_COLOR = 'rgba(0, 0, 255, 0.2)'
 
 const AudioVisualizers: FC = () => {
   const { selectedVideo, videoRef, spliceRegions, modifySpliceRegion, deleteSpliceRegion } = useSpliceVideo()
@@ -34,6 +35,8 @@ const AudioVisualizers: FC = () => {
   const [activeRegion, setActiveRegion] = useState<Region>()
   const [isLoop, setIsLoop] = useState(false)
   const [isPlaying, setIsPlaying] = useState(false)
+
+  const [editNameModalOpen, setEditNameModalOpen] = useState(false)
 
   const handlePlayPause = () => {
     // find selectedRegion in region
@@ -183,103 +186,183 @@ const AudioVisualizers: FC = () => {
   }, [videoRef, regions])
 
   return (
-    <Box>
-      <Stack direction='row' alignItems='center' justifyContent='space-between' mb={2}>
-        <Stack direction='row' alignItems='center' spacing={1}>
-          <Typography>
-            <b>Selected Splice Region:</b> {selectedRegion ? selectedRegion.name : 'None'}
-          </Typography>
-          <IconButton size='small' disabled={!selectedRegion}>
-            <Edit />
-          </IconButton>
+    <>
+      <Box>
+        <Stack direction='row' alignItems='center' justifyContent='space-between' mb={2}>
+          <Stack direction='row' alignItems='center' spacing={1}>
+            {
+              selectedRegion &&
+              <>
+                <Typography>
+                  <b>Selected Splice Region:</b> {selectedRegion.name}
+                </Typography>
+                <IconButton size='small' onClick={() => setEditNameModalOpen(true)}>
+                  <Edit />
+                </IconButton>
+              </>
+            }
+          </Stack>
+          <Box>
+            <IconButton onClick={handlePlayPause}
+              disabled={!selectedRegion}
+            >
+              {isPlaying ? <Pause /> : <PlayArrow />}
+            </IconButton>
+            <IconButton
+              onClick={() => setIsLoop(!isLoop)}
+              disabled={!selectedRegion}
+              color={isLoop ? 'primary' : 'default'}
+            >
+              <Loop />
+            </IconButton>
+            <IconButton onClick={() => deleteSpliceRegion(selectedRegion!)} disabled={!selectedRegion} color='error'>
+              <Delete />
+            </IconButton>
+          </Box>
         </Stack>
-        <Box>
-          <IconButton onClick={handlePlayPause}
-            disabled={!selectedRegion}
-          >
-            {isPlaying ? <Pause /> : <PlayArrow />}
-          </IconButton>
-          <IconButton
-            onClick={() => setIsLoop(!isLoop)}
-            disabled={!selectedRegion}
-            color={isLoop ? 'primary' : 'default'}
-          >
-            <Loop />
-          </IconButton>
-          <IconButton onClick={() => deleteSpliceRegion(selectedRegion!)} disabled={!selectedRegion} color='error'>
-            <Delete />
-          </IconButton>
-        </Box>
-      </Stack>
-      <Box
-        sx={{
-          height: 'calc(100vh - 64px - 128px - 80px)',
-          overflowY: 'auto',
-          pb: 12,
-        }}
-      >
-        <WavesurferPlayer
-          height={256}
-          media={videoRef!}
-          progressColor='#1976d2'
-          minPxPerSec={zoom}
-          dragToSeek
-          normalize
-          // @ts-ignore
-          splitChannels
-          sampleRate={audioSampleRate}
-          frequencyMax={frequencyMax}
-          onDecode={(wavesurfer) => {
-            wavesurfer.registerPlugin(TimelinePlugin.create({
-              secondaryLabelOpacity: 1,
-              style: 'font-size: 12px'
-            }))
-
-            wavesurfer.registerPlugin(SpectrogramPlugin.create({
-              labels: true,
-              frequencyMax,
-              labelsBackground: '#00000066',
-              colorMap: COLORS,
-              splitChannels: false,
-            }))
-
-            const wsRegions = wavesurfer.registerPlugin(RegionPlugin.create())
-            setWsRegions(wsRegions)
+        <Box
+          sx={{
+            height: 'calc(100vh - 64px - 128px - 80px)',
+            overflowY: 'auto',
+            pb: 12,
           }}
-          onInteraction={handleInteraction}
-        />
-        <Box maxWidth='calc(100% - 32px)'>
-          <Stack direction='row' alignItems='center' gap={2}>
-            <Typography variant='body2'>
-              Zoom
-            </Typography>
-            <Slider
-              value={zoom}
-              onChange={(_, value) => setZoom(value as number)}
-              min={1}
-              max={5000}
-              disabled={!selectedVideo}
-            />
-          </Stack>
-          <Stack direction='row' alignItems='center' gap={2}>
-            <Typography variant='body2' noWrap>
-              Max Frequency (Hz)
-            </Typography>
-            <Slider
-              value={frequencyMax}
-              onChange={(_, value) => setFrequencyMax(value as number)}
-              min={0}
-              max={audioSampleRate / 2}
-              valueLabelDisplay='auto'
-              sx={{
-                flex: 1,
-              }}
-              disabled={!selectedVideo}
-            />
-          </Stack>
+        >
+          <WavesurferPlayer
+            height={256}
+            media={videoRef!}
+            progressColor='#1976d2'
+            minPxPerSec={zoom}
+            dragToSeek
+            normalize
+            // @ts-ignore
+            splitChannels
+            sampleRate={audioSampleRate}
+            frequencyMax={frequencyMax}
+            onDecode={(wavesurfer) => {
+              wavesurfer.registerPlugin(TimelinePlugin.create({
+                secondaryLabelOpacity: 1,
+                style: 'font-size: 12px'
+              }))
+
+              wavesurfer.registerPlugin(SpectrogramPlugin.create({
+                labels: true,
+                frequencyMax,
+                labelsBackground: '#00000066',
+                colorMap: COLORS,
+                splitChannels: false,
+              }))
+
+              const wsRegions = wavesurfer.registerPlugin(RegionPlugin.create())
+              setWsRegions(wsRegions)
+            }}
+            onInteraction={handleInteraction}
+          />
+          <Box maxWidth='calc(100% - 32px)'>
+            <Stack direction='row' alignItems='center' gap={2}>
+              <Typography variant='body2'>
+                Zoom
+              </Typography>
+              <Slider
+                value={zoom}
+                onChange={(_, value) => setZoom(value as number)}
+                min={1}
+                max={5000}
+                disabled={!selectedVideo}
+              />
+            </Stack>
+            <Stack direction='row' alignItems='center' gap={2}>
+              <Typography variant='body2' noWrap>
+                Max Frequency (Hz)
+              </Typography>
+              <Slider
+                value={frequencyMax}
+                onChange={(_, value) => setFrequencyMax(value as number)}
+                min={0}
+                max={audioSampleRate / 2}
+                valueLabelDisplay='auto'
+                sx={{
+                  flex: 1,
+                }}
+                disabled={!selectedVideo}
+              />
+            </Stack>
+          </Box>
         </Box>
       </Box>
-    </Box>
+      {
+        selectedRegion && editNameModalOpen &&
+        <EditNameModal
+          open={editNameModalOpen}
+          onClose={() => setEditNameModalOpen(false)}
+          spliceRegion={selectedRegion}
+        />
+      }
+    </>
+  )
+}
+
+interface EditNameModalProps {
+  open: boolean
+  onClose: () => void
+  spliceRegion: SpliceRegion
+}
+
+const EditNameModal: FC<EditNameModalProps> = ({
+  open,
+  onClose,
+  spliceRegion,
+}) => {
+  const { spliceRegions, modifySpliceRegion } = useSpliceVideo()
+
+  const [name, setName] = useState<string>(spliceRegion.name)
+  const [helperText, setHelperText] = useState<string>('')
+
+  const handleChangeName = () => {
+    // if name is not unique, return
+    if (spliceRegions.find(region => region.name === name)) {
+      setHelperText('Name must be unique')
+      return
+    }
+
+    modifySpliceRegion(spliceRegion, {
+      ...spliceRegion,
+      name,
+    })
+
+    onClose()
+  }
+
+  return (
+    <Modal open={open} onClose={onClose}>
+      <Box
+        gap={2}
+        display='flex'
+        flexDirection='column'
+        component='form'
+        onSubmit={e => {
+          e.preventDefault()
+          handleChangeName()
+        }}
+      >
+        <Typography variant='h5'>
+          Edit Splice Region Name
+        </Typography>
+        <TextField
+          label='Name'
+          value={name}
+          onChange={e => {
+            setName(e.target.value)
+            setHelperText('')
+          }}
+          fullWidth
+          error={!!helperText}
+          helperText={helperText}
+        />
+        <Button variant='contained' fullWidth type='submit'>
+          Save
+        </Button>
+      </Box>
+    </Modal>
   )
 }
 
