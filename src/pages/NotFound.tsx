6a36019b83d5e23cb6beacb288838a67d1217dcb
Phillip Tran <ptrandev@protonmail.com>
2024-03-25 00:13:45 -0400

    modified not found page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -4,13 +4,13 @@ import { NavLink } from 'react-router-dom'
 
 const NotFound: FC = () => {
   return (
-    <Stack alignItems='center' justifyContent='center' width='100%' height='100vh' flexDirection='column' gap={2} p={4}>
+    <Stack alignItems='center' justifyContent='center' width='100%' height='100vh' flexDirection='column' gap={4} p={4}>
       <Stack alignItems='center'>
         <Typography variant='h3' textAlign='center'>
           Not Found
         </Typography>
-        <Box maxWidth='500px'>
-          <img src='src/assets/NotFound.png' alt='404 Not Found' width='100%' />
+        <Box maxWidth='500px' py={2}>
+          <img src='src/assets/NotFound.svg' alt='404 Not Found' width='100%' />
         </Box>
         <Typography variant='body1' textAlign='center'>
           The page you are looking for does not exist.
